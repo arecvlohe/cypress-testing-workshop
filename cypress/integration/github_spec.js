5d537ce17d7ff164b9adf1e2d6feb37cafc37941
Adam Recvlohe <adam.recvlohe@gmail.com>
2018-05-22 18:12:57 -0400

    Expect and aliases

--- cypress/integration/github_spec.js
+++ cypress/integration/github_spec.js
@@ -1,11 +1,21 @@
 describe("personal portfolio", function() {
   before(function() {
     cy.visit("/arecvlohe");
+
+    cy
+      .get("li.vcard-detail.pt-1.css-truncate.css-truncate-target")
+      .eq(2)
+      .invoke("text")
+      .as("signInToView");
   });
 
   it("should - render the number of repos I have", function() {
     cy
       .get("a.UnderlineNav-item[title=Repositories] > span.Counter")
-      .contains("105");
+      .contains("106");
+  });
+
+  it("should - not render the users email when not logged in", function() {
+    expect(this.signInToView.trim()).to.eq("Sign in to view email");
   });
 });
